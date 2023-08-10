fc76674cec6f311a02018bf87ee6818befa70631
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-08-11 01:30:44 +0600

    Conditional formatting on signin and signout

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 "# Simple-Firebase-Authentication" 
-"# Simple-Firebase-Authentication" 
+
+-----------------------
+INITIAL SETUP
+-----------------------
 1. visit:  console.firebase.google.com
 2. create a new firebase project
 3. Visit doc(go to docs): Build->Authentication->Web->Getting started
@@ -7,6 +10,9 @@
 5. Install firebase for your project: npm install firebase
 6. get firebase config and put it in firebase.init.js
 7. export app from firebase.init.js
+----------------------
+SETUP THE PROVIDER
+----------------------
 8. create auth using getAuth from firebase by using app from firebase.init.js
 9. create a google auth provider
 10. go to firebase -> build -> Authentication -> sign in method
@@ -14,3 +20,7 @@
 12. create a button for google sign method with a click handler
 13. inside the event handler, call signInWithPopup with auth, provider
 14. after signInWithPopup .then result, error
+
+----------------------
+DISPLAY DATA
+----------------------
--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import './App.css';
-import { GoogleAuthProvider, getAuth, signInWithPopup} from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut} from "firebase/auth";
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 const auth = getAuth(app)
 
 function App() {
+  const [user, setUser]= useState({})
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () =>{
     signInWithPopup(auth, provider)
     .then(result=>{
       const user = result.user;
+      setUser(user);
     })
     .catch(error=>{
       console.log("error: ", error);
     })
   }
+
+  const handleGoogleSignOut = () =>{
+    signOut(auth)
+    .then(()=>{
+      setUser({})
+    })
+    .catch(()=>{
+      setUser({})
+    })
+  }
+
   return (
     <div className="App">
-     <button onClick={handleGoogleSignIn}>Google Sign In</button>
+     { user.email?
+     <button onClick={handleGoogleSignOut}>Sign Out</button>:
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+     }
+     {user.email && <div>
+      <h3>User name: {user.displayName}</h3>
+      <p>Email address: {user.email}</p>
+      <img src={user.photoURL} alt="" />
+     </div>}
     </div>
   );
 }
